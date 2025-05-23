2979ca554076a79dea044aae51efd020d04abef6
人間工作 <coolxiaom95@gmail.com>
2025-05-23 20:48:18 +0800

    kotlin plugin is unnecessary

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,7 +7,6 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.22")
     implementation("gradle.plugin.com.hierynomus.gradle.plugins:license-gradle-plugin:0.16.1")
     implementation("com.gradleup.shadow:shadow-gradle-plugin:8.3.0")
 }
